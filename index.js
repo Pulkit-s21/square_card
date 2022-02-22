8fca372dc550206782a4234eafd39bea51a3b6f0
Pulkit-s21 <pulkitsaxena795@gmail.com>
2022-02-22 20:15:48 +0530

    sqaure box to open up the webpage in it

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", () => {
+    window.open("https://www.youtube.com");
+});
+
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", () => {
+    window.open("https://www.youtube.com");
+});
+
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", () => {
+    window.open("https://www.youtube.com");
+});
+
+document.getElementsByClassName("fab fa-youtube")[3].addEventListener("click", () => {
+    window.open("https://www.youtube.com");
+});
+
+document.getElementsByClassName("article")[0].addEventListener("click", () => {
+    window.open("https://github.com/Pulkit-s21");
+});
+
+document.getElementsByClassName("article")[2].addEventListener("click", () => {
+    window.open("https://github.com/Pulkit-s21");
+});
